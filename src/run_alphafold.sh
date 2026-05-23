bef854e66635be4cb54e7529276d102766b493aa
PCtryhard <maximilian.alexandrov@hotmail.com>
2026-05-23 10:53:46 +0800

    Update run_alphafold.sh

--- src/run_alphafold.sh
+++ src/run_alphafold.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Prevent JAX from swallowing all GPU VRAM, but remove the unified memory flag
-# to prevent WSL from segfaulting during system RAM paging
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 
-# Run ColabFold using the GPU, restricted to 1 model for rapid screening
+# Run ColabFold with 0 recycles to bypass the WSL memory crash
 localcolabfold/colabfold-conda/bin/colabfold_batch \
     data/processed/mpnn/seqs \
     data/processed/alphafold \
-    --num-recycle 3 \
+    --num-recycle 0 \
     --num-models 1 \
     --model-type alphafold2_ptm
