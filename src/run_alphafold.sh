f563b845f345543f74dae77b6b069aedc04d57d9
PCtryhard <maximilian.alexandrov@hotmail.com>
2026-05-23 10:22:19 +0800

    Update run_alphafold.sh

--- src/run_alphafold.sh
+++ src/run_alphafold.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Update the input path to wherever the actual files are!
+# Force JAX to allocate memory dynamically to prevent WSL crashes
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export TF_FORCE_UNIFIED_MEMORY=1
+
+# Run ColabFold using the GPU
 localcolabfold/colabfold-conda/bin/colabfold_batch \
     data/processed/mpnn/seqs \
     data/processed/alphafold \
